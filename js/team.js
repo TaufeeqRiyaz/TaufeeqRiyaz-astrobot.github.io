2d24caf9aa5efb85c67808f4c989840151a039de
Taufeeq Riyaz <taufeeq.riyaz64@gmail.com>
2021-08-03 01:25:14 +0530

    .

--- js/team.js
+++ js/team.js
@@ -5,7 +5,7 @@ const API = "https://discord-web-api.glitch.me/discord/user/";
 const owners = [
     {
         "id": "810785951664046120",
-        "post": "Bot Owner",
+        "post": "Bot Devloper",
         "GHURL": "https://github.com/TaufeeqRiyaz",
         "InstaURL": "https://instagram.com/taufeeq_riyaz_official",
     }
@@ -22,7 +22,7 @@ for (let indexOne = 0; indexOne < owners.length; indexOne++) {
                 }, 1000);
             }
 
-            const ownerList = "<div id='trigger' class='card' style='margin: 15px;'><div class='banner'><img src='" + output.url + "'></div></br></br></br></br><h2 class='name'>" + output.username + "</h2><div class='title'><h1 id='trigger2' style='font-size: 18px; color: #fffff;'>" + elementOwners.post + "</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='" + elementOwners.GHURL + "' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'><a href='" + elementOwners.InstaURL + "' target='_blank'><button style='color: #000000;'>Instagram</button></a></div></div></div>"
+            const ownerList = "<div id='trigger' class='card' style='margin: 15px;'><div class='banner'><img src='" + "https://cdn.discordapp.com/attachments/851805970987876404/871843345314500629/av.jpg"+ "'></div></br></br></br></br><h2 class='name'>" + "Astrogeek" + "</h2><div class='title'><h1 id='trigger2' style='font-size: 18px; color: #fffff;'>" + elementOwners.post + "</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='" + elementOwners.GHURL + "' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'><a href='" + elementOwners.InstaURL + "' target='_blank'><button style='color: #000000;'>Instagram</button></a></div></div></div>"
             boxOwners.innerHTML += ownerList;
         });
 }
